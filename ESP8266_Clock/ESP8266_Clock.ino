050b10e2f4af112ae344816beb9b9b4632824bc4
Dragan Knežević <tomaja@tomaja.com>
2021-10-16 22:16:54 +0200

    AP mode added

--- ESP8266_Clock/ESP8266_Clock.ino
+++ ESP8266_Clock/ESP8266_Clock.ino
@@ -1,5 +1,6 @@
 #include <ArduinoJson.h>
 
+#include "EEPROM.h"
 #include <NTPClient.h>
 // change next line to use with another board/shield
 #include <ESP8266WiFi.h>
@@ -18,16 +19,30 @@
 enum Mode { TIME, IP, TEMP };
 Mode mode = Mode::IP;
 
-const char *ssid     = "tomaje";
-const char *password = "kol1kopter123";
+char *ssid     = "tomaje";
+char *password = "kol1kopter123";
+
+char zsSsid[32] = { 0 };
+char zsPass[32] = { 0 };
 
 char szTemp[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 std::vector<std::string> vParts;  
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 7200, 60000);
 
 ESP8266WebServer server(80);
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>WiFi Settings</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head><body>
+  <form action="/wifi">
+    SSID: <input type="text" name="ssid"><br />
+    Password: <input type="text" name="password"><br />
+    <input type="submit" value="Save">
+  </form>
+</body></html>)rawliteral";
 
 void handleWeather()
 {
@@ -82,6 +97,60 @@ void handleIP()
 {
   mode = Mode::IP;
 }
+
+void ResetSettingsFlash()
+{
+  // write a 0 to all 512 bytes of the EEPROM
+  for (int i = 0; i < 512; i++) {
+    EEPROM.write(i, 0xCD);
+  }
+}
+
+void ReadStringFromFlash(int& nPos, char* zsString)
+{
+  char ch;
+  int i = 0;
+  while (ch = EEPROM.read(nPos++))
+  {
+    zsString[i++] = ch;
+  }
+  zsString[i] = 0;
+}
+
+void ReadSettingsFlash()
+{
+  EEPROM.begin(512);
+  int nPosition = 0;
+  ReadStringFromFlash(nPosition, zsSsid);
+  ReadStringFromFlash(nPosition, zsPass);
+  EEPROM.end();
+}
+
+void WriteStringToFlash(const char* strString, int& nPointer)
+{
+  Serial.print ( "Writing string: " );
+  char ch;
+  int nLen = strlen(strString);
+  for (int i = 0; i < nLen; i++)
+  {
+    Serial.print ( strString[i] );
+    EEPROM.write(nPointer++, strString[i]);
+  }
+  Serial.print ( "\n" );
+  EEPROM.write(nPointer++, 0);
+}
+
+void WriteSettingsFlash()
+{
+  EEPROM.begin(512);
+  int nPosition = 0;
+  WriteStringToFlash(zsSsid, nPosition);
+  WriteStringToFlash(zsPass, nPosition);
+  EEPROM.end();
+}
+
+bool bAPMode = true;
+
 void setup()
 {
   ESP.wdtDisable();
@@ -90,33 +159,132 @@ void setup()
   pinMode(BUFFER_LATCH, OUTPUT);
   
   Serial.begin(115200);
-
-  WiFi.begin(ssid, password);
-
-  int count = 0;
-  std::string strInit = "";
-  while ( WiFi.status() != WL_CONNECTED )
-  {
-    delay ( 40 );
-    Serial.print ( "." );
-    strInit += '0';
-    PrintForMS(strInit, 200);
-    if(strInit == "00000000")
-    {
-      strInit = "";
-    }
-  }
-  IPAddress ip;
-  ip = WiFi.localIP();
-  Serial.println(ip);
-  ESP.wdtFeed();
-  vParts = Split(std::string(ip.toString().c_str()), '.');
+  Serial.print ( "Loading settings\n" );
   
-  server.on("/w", handleWeather);
-  server.on("/t", [&](){ mode == Mode::TIME; return true; });
-  server.on("/i", handleIP);
-  server.begin();
-  timeClient.begin();
+  EEPROM.begin(512);
+  //ResetSettingsFlash();
+  char ch = EEPROM.read(0);
+  EEPROM.end();
+  
+  if(ch != 0xCD)
+  {
+    bAPMode = false;
+    ReadSettingsFlash();   
+    Serial.print ( zsSsid );
+    Serial.print ( "\n" );
+    Serial.print ( zsPass );
+    Serial.print ( "\n" );
+    WiFi.begin(zsSsid, zsPass);
+    
+    int count = 0;
+    std::string strInit = "";
+    while ( WiFi.status() != WL_CONNECTED )
+    {
+      delay ( 40 );
+      Serial.print ( "." );
+      strInit += '0';
+      PrintForMS(strInit, 200);
+      if(strInit == "00000000")
+      {
+        strInit = "";
+      }
+      count++;
+      if(count > 100)
+      {
+        break;
+      }
+    }
+
+    if(WiFi.status() != WL_CONNECTED)
+    {
+      boolean result = WiFi.softAP("espSat", "12345678");
+      if(result == true)
+      {
+        Serial.println("Ready");
+      
+        IPAddress myIP = WiFi.softAPIP();
+        Serial.print("AP IP address: ");
+        Serial.println(myIP);
+        server.on("/", [&](){ server.send(200, "text/html", index_html); });
+        server.on("/wifi", [&](){
+          char zsChars[32] = {0};
+          server.arg("ssid").toCharArray(zsChars, 32);
+          strcpy(zsSsid, zsChars);
+          server.arg("password").toCharArray(zsChars, 32);
+          strcpy(zsPass, zsChars);
+          WriteSettingsFlash();
+          delay(1000);
+          ESP.restart();
+          });
+        server.begin();
+        bAPMode = true;
+        Serial.println("HTTP server started");
+        return;        
+      }
+      else
+      {
+        Serial.println("Failed!");
+      }
+    }
+    
+    IPAddress ip;
+    ip = WiFi.localIP();
+    Serial.println(ip);
+    ESP.wdtFeed();
+    vParts = Split(std::string(ip.toString().c_str()), '.');
+    
+    server.on("/w", handleWeather);
+    server.on("/t", [&](){ mode == Mode::TIME; return true; });
+    server.on("/i", handleIP);
+    server.on("/setup", [&](){ server.send(200, "text/html", index_html); });
+    server.on("/wifi", [&](){
+      char zsChars[32] = {0};
+      server.arg("ssid").toCharArray(zsChars, 32);
+      strcpy(zsSsid, zsChars);
+      Serial.println(zsSsid);
+      server.arg("password").toCharArray(zsChars, 32);
+      strcpy(zsPass, zsChars);
+      Serial.println(zsPass);
+      WriteSettingsFlash();
+      ESP.restart();
+      });
+    server.begin();
+    timeClient.begin();
+  }
+  else
+  {
+    bAPMode = true;
+    boolean result = WiFi.softAP("espSat", "12345678");
+    if(result == true)
+    {
+      Serial.println("Ready");
+    
+      IPAddress myIP = WiFi.softAPIP();
+      Serial.print("AP IP address: ");
+      Serial.println(myIP);
+      server.on("/", [&](){ server.send(200, "text/html", index_html); });
+      server.on("/wifi", [&](){
+        char zsChars[32] = {0};
+        server.arg("ssid").toCharArray(zsChars, 32);
+        strcpy(zsSsid, zsChars);
+        server.arg("password").toCharArray(zsChars, 32);
+        strcpy(zsPass, zsChars);
+        WriteSettingsFlash();
+        delay(1000);
+        ESP.restart();
+        });
+      server.begin();
+      Serial.println("HTTP server started");
+      
+    }
+    else
+    {
+      Serial.println("Failed!");
+    }
+    strcpy(zsSsid, ssid);
+    strcpy(zsPass, password);
+    WriteSettingsFlash();
+  }
 }
 
 void loop()
@@ -124,7 +292,7 @@ void loop()
   timeClient.update();
   server.handleClient();
 
-  if(true)
+  if(!bAPMode)
   {
     if(mode == Mode::IP)
     {
@@ -149,14 +317,6 @@ void loop()
   }
   else // TEST
   {
-    PrintChar('0', 0, 1);
-    PrintChar('1', 1, 0);
-    PrintChar('2', 2, 1);
-    PrintChar('3', 3, 0);
-    PrintChar('4', 4, 1);
-    PrintChar('5', 5, 0);
-    PrintChar('6', 6, 1);
-    PrintChar('7', 7, 0);
-    PrintChar('8', 8, 1);
+    PrintChar('AP      ', 0, 1);
   }
 }
